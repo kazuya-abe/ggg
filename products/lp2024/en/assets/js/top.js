e0abd89a8ab80840fd55ca3b63735619cf685971
kazuya-abe <abe@ossi.co.jp>
2024-02-06 15:49:10 +0900

    del

--- products/lp2024/en/assets/js/top.js
+++
@@ -1,4 +0,0 @@
-var swiper = new Swiper(".-slide1 .sec4_swiper", {
-loop: true,slidesPerView:'auto',spaceBetween: 0,autoplay:{delay:4000},speed:1100,pagination: {el: ".-slide1 .swiper-pagination"}});
-var swiper = new Swiper(".-slide2 .sec4_swiper", {
-loop: true,slidesPerView:'auto',spaceBetween: 0,autoplay:{delay:4000},speed:1100,pagination: {el: ".-slide2 .swiper-pagination"}});
